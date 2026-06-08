1366a83d9bd27cc9cea1c2dec024b0ab431a7682
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2026-06-07 22:39:30 -0300

    se agregan los primeros handlers del servidor

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessHandler)
+
 	myServer := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
 	myServer.ListenAndServe()
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
